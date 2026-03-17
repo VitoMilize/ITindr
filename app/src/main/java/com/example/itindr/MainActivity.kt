c6550c1ae8cf8a5eb79c9a2c924eb19d789c4da5
Victor Maslov <maslovvial04@gmail.com>
2026-03-17 07:32:54 +0700

    Сверстал экран сплэш скрина, добавил пустой экран выбора регистарции или логина

--- app/src/main/java/com/example/itindr/MainActivity.kt
+++ app/src/main/java/com/example/itindr/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.splash_screen)
     }
 }
